fe3c56fca2df731cf67e6fbeca2fda397091e5f1
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-29 19:10:17 +0500

    use SUM, COUNT, GROUP BY and HAVING aggregates

--- practice.sql
+++ practice.sql
@@ -99,3 +99,28 @@ WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND popu
 SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany') * 100, 0), '%') FROM world
 WHERE continent = 'Europe'
 
+SELECT SUM(population)
+FROM world
+
+SELECT DISTINCT continent FROM world
+
+SELECT SUM(GDP) FROM world
+WHERE continent = 'Africa'
+
+SELECT COUNT(name) FROM world
+WHERE area >= 1000000
+
+SELECT SUM(population) FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+
+SELECT continent, COUNT(name) FROM world
+GROUP BY continent
+
+SELECT continent, COUNT(name) FROM world
+WHERE population >= 10000000
+GROUP BY continent
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
