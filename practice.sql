aa968013a3a288e94d5c678a6967cbca09840082
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-29 17:36:29 +0500

    use LENGTH and ROUND functions

--- practice.sql
+++ practice.sql
@@ -60,3 +60,12 @@ WHERE (area >= 3000000) OR (population >= 250000000)
 
 SELECT name, population, area FROM world
 WHERE (area >= 3000000) XOR (population >= 250000000)
+
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2) FROM world
+WHERE continent = 'South America'
+
+SELECT name, ROUND(GDP/population, -3) FROM world
+WHERE GDP >= 1000000000000
+
+SELECT name, capital FROM world
+WHERE LENGTH(name) = LENGTH(capital)
