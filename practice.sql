4b092760e6b472e1cbcddad66926b725ac8e782e
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-29 17:31:51 +0500

    use SELECT with WHERE AND IN operators

--- practice.sql
+++ practice.sql
@@ -38,3 +38,25 @@ SELECT name FROM world
 SELECT name FROM world
  WHERE name LIKE '____'
 
+SELECT name, continent, population FROM world
+
+SELECT name FROM world
+WHERE population >= 200000000
+
+SELECT name, GDP/population FROM world
+WHERE population >= 200000000
+
+SELECT name, population/1000000 FROM world
+WHERE continent = 'South America'
+
+SELECT name, population FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+SELECT name FROM world
+WHERE name LIKE ('%United%')
+
+SELECT name, population, area FROM world
+WHERE (area >= 3000000) OR (population >= 250000000)
+
+SELECT name, population, area FROM world
+WHERE (area >= 3000000) XOR (population >= 250000000)
