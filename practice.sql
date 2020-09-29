feb755c50bf46f7388dfa99d77564306ad055c23
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-29 17:11:00 +0500

    use LIKE along with SELECT

--- practice.sql
+++ practice.sql
@@ -6,3 +6,35 @@ SELECT name, population FROM world
 
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'C%' AND name LIKE '%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+SELECT name FROM world
+ WHERE name LIKE '____'
+
